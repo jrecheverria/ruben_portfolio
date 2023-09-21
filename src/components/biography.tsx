112b834f7defc13b807ccd3e3a9dea57f0bef8f0
Javier Echeverria <jrecheverria@protonmail.com>
2023-09-21 12:51:01 -0400

    Added some place holder values for my currently doing section. I'll eventually add some automated way to update these

--- src/components/biography.tsx
+++ src/components/biography.tsx
@@ -1,16 +1,23 @@
+import Socials from "./socials";
+
 export default function Biography() {
     return (
         <div className="flex flex-col pt-10 w-11/12 sm:w-6/12">
             <h1 className="font-ibm text-3xl sm:text-5xl font-bold">Ruben Echeverria</h1>
             <p className="font-ibm text-md font-light pt-9">
-            Hey 👋 Im Ruben Echeverria, a software engineer who is deeply passionate about all things coding.
+            Hey 👋 Im Ruben Echeverria, a software engineer who is deeply passionate about all things coding. Currently,
+            Im working at CitiBank managing their artifact management platform and designing micro-services in Java ☕️.
             This portfolio website serves as a platform to showcase my projects and share things Im currently learning. 
             </p>
             
-            <p className="font-ibm text-md font-light pt-5">Social Links:</p>
-            <p className="font-ibm text-md font-light pt-5">Currently Learning / Reading:</p>
-            <p className="font-ibm text-md font-light pt-5">Currently Listening To:</p>
-            <p className="font-ibm text-md font-light pt-5">Actively Working On:</p>
+            <div className="flex items-center content-center">
+                <p className="font-ibm text-md font-light pt-5">Social Links:</p>
+                <Socials />
+            </div>
+            
+            <p className="font-ibm text-md font-light pt-5">Currently Learning / Reading: <span className="font-medium">Creating SQLite from Scratch - CodeCrafters</span></p>
+            <p className="font-ibm text-md font-light pt-5">Currently Listening To: <span className="font-medium">Mansard Roof - Vampire Weekend</span></p>
+            <p className="font-ibm text-md font-light pt-5">Actively Working On: <span className="font-medium">This Portfolio Site</span></p>
             <hr className="h-px my-8 bg-orange-600 border-0"></hr>
         </div>
     )
--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -8,22 +8,22 @@ interface SocialObject {
 
 const socials = [
     {
-        link: "https://github.com/jrecheverria?tab=repositories",
-        icon: <AiOutlineGithub className="w-8 h-8 cursor-pointer text-slate-500 hover:text-white"/>
+        link: "https://github.com/jrecheverria",
+        icon: <AiOutlineGithub className="w-7 h-7 cursor-pointer text-slate-500 hover:text-orange-600"/>
     },
     {
         link: "https://www.instagram.com/rubenecheverria/",
-        icon: <AiOutlineInstagram className="w-8 h-8 cursor-pointer text-slate-500 hover:text-white"/>
+        icon: <AiOutlineInstagram className="w-7 h-7 cursor-pointer text-slate-500 hover:text-orange-600"/>
     },
     {
         link: "https://www.linkedin.com/in/javier-echeverria-780617195/",
-        icon: <AiOutlineLinkedin className="w-8 h-8 cursor-pointer text-slate-500 hover:text-white"/>
+        icon: <AiOutlineLinkedin className="w-7 h-7 cursor-pointer text-slate-500 hover:text-orange-600"/>
     }
 ]
 
 export default function Socials() {
     return (
-        <div className="flex mt-40">
+        <div className="flex">
         {socials.map((social, index) => (
             <div key={index} className="mr-4">
                 <a href={social.link} target="_blank">{social.icon}</a>
